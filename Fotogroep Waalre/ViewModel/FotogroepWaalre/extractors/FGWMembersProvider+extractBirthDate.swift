bdf2ec9febbf70291d3d57b00e8c0fe9c2000bb4
Peter van den Hamer <github@vdhamer.com>
2022-12-27 11:49:27 +0100

    Updated extractBirthDate to Swift 5.7 Regex

--- Fotogroep Waalre/ViewModel/FotogroepWaalre/extractors/FGWMembersProvider+extractBirthDate.swift
+++ Fotogroep Waalre/ViewModel/FotogroepWaalre/extractors/FGWMembersProvider+extractBirthDate.swift
@@ -12,6 +12,36 @@ extension FGWMembersProvider {
 
     func extractBirthDate(taggedString: String) -> Date? {
         // <td>2022-05-26</td> is valid input
+//        let regex_old: Regex = /"<td>(.*)</td>"/
+
+        let birthDate: String
+
+        let regex = Regex {
+            "<td>"
+            Capture {
+                ZeroOrMore(.any, .reluctant)
+            }
+            "</td>"
+        }
+
+        if let match = try? regex.wholeMatch(in: taggedString) {
+            let (_, dateString) = match.output
+            birthDate = String(dateString)
+        } else {
+            fatalError("Failed to decode date from \(taggedString) because RegEx didn't trigger")
+        }
+
+        let strategy = Date.ParseStrategy(format: "\(year: .defaultDigits)-\(month: .twoDigits)-\(day: .twoDigits)",
+                                          timeZone: TimeZone.autoupdatingCurrent)
+        let date = try? Date(birthDate, strategy: strategy) // can be nil
+        if date==nil && !birthDate.isEmpty {
+            print("Failed to decode data from \"\(birthDate)\" because the date is not in ISO8601 format")
+        }
+        return date
+    }
+
+    func extractBirthDate_old(taggedString: String) -> Date? { // TODO: remove
+        // <td>2022-05-26</td> is valid input
 
         let REGEX: String = "<td>(.*)</td>"
         let result = taggedString.capturedGroups(withRegex: REGEX)
