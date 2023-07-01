09c0adaf5ea620deb44ea171c866ac8a79133652
Peter van den Hamer <github@vdhamer.com>
2023-07-01 19:29:19 +0200

    Migrated from fatelError() to ifDebugFatalError()

--- Fotogroep Waalre/View/PhotoClubsView/PhotoClubView.swift
+++ Fotogroep Waalre/View/PhotoClubsView/PhotoClubView.swift
@@ -140,10 +140,11 @@ struct PhotoClubView: View {
                 }
             } catch {
                 // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate.
                 // You should not use this function in a shipping application
                 let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                ifDebugFatalError("Unresolved error \(nsError), \(nsError.userInfo)",
+                                  file: #fileID, line: #line) // likely deprecation of #fileID in Swift 6.0
+                // in release mode, the failed deletion is only logged. App doesn't stop.
             }
         }
 
