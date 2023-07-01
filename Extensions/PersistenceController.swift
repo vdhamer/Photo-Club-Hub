9039c0f4f82626e007a0e08908720639984da805
Peter van den Hamer <github@vdhamer.com>
2023-07-01 23:57:22 +0200

    Cleanup around fatalError() calls

--- Extensions/PersistenceController.swift
+++ Extensions/PersistenceController.swift
@@ -10,7 +10,6 @@ import CoreLocation // needed for coordinate translation
 
 struct PersistenceController {
 	static let shared = PersistenceController()
-//    let storename = String("Foto Club Hub") // Used as Core Data database name - not localized.
 
 	static var preview: PersistenceController = {
 		let result = PersistenceController(inMemory: true)
@@ -50,11 +49,8 @@ struct PersistenceController {
 		do {
 			try viewContext.save()
 		} catch {
-			// Replace this implementation with code to handle the error appropriately.
-			// fatalError() causes the application to generate a crash log and terminate.
-			// You should not use this function in a shipping application, although it may be useful during development.
 			let nsError = error as NSError
-			fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+			fatalError("Unresolved error \(nsError), \(nsError.userInfo)") // preview cannot occur in shipping code
 		}
 		return result
 	}()
@@ -80,20 +76,7 @@ struct PersistenceController {
         container.viewContext.automaticallyMergesChangesFromParent = true
         container.loadPersistentStores(completionHandler: { (_ storeDescription, error) in
 			if let error = (error as NSError?) {
-				// Replace this implementation with code to handle the error appropriately.
-				// fatalError() causes the application to generate a crash log and terminate.
-                // You should not use this function in a shipping application,
-                // although it may be useful during development.
-
-				/*
-				Typical reasons for an error here include:
-				* The parent directory does not exist, cannot be created, or disallows writing.
-				* The persistent store is not accessible, due to permissions or data protection when the device is locked.
-				* The device is out of space.
-				* The store could not be migrated to the current model version.
-				Check the error message to determine what the actual problem was.
-				*/
-				fatalError("Unresolved error \(error), \(error.userInfo)")
+				fatalError("Unresolved error \(error), \(error.userInfo)") // don't know how to recover
             }
 		})
 	}
