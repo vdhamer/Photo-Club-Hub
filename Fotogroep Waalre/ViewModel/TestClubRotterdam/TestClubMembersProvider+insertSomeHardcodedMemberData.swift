a9c2302b0845ee49f2431b7fd9a9b8cd7bb9e2ca
Peter van den Hamer <github@vdhamer.com>
2023-07-26 00:31:37 +0200

    GPS coordinate chanage for Test Photo Club Rotterdam

--- Fotogroep Waalre/ViewModel/TestClubRotterdam/TestClubMembersProvider+insertSomeHardcodedMemberData.swift
+++ Fotogroep Waalre/ViewModel/TestClubRotterdam/TestClubMembersProvider+insertSomeHardcodedMemberData.swift
@@ -33,8 +33,8 @@ extension TestClubRotterdamMembersProvider { // fill with some initial hard-code
                                              photoClubIdPlus: Self.photoClubTestRotterdamIdPlus,
                                              photoClubWebsite: TestClubRotterdamMembersProvider.testRotterdamURL,
                                              fotobondNumber: nil, kvkNumber: nil,
-                                             coordinates: CLLocationCoordinate2D(latitude: 51.905292,
-                                                                                 longitude: 4.486934),
+                                             coordinates: CLLocationCoordinate2D(latitude: 51.90296,
+                                                                                 longitude: 4.49504),
                                              priority: 1
                                             )
         clubTestRotterdam.hasHardCodedMemberData = true // store in database that we ran insertSomeHardcodedMembers...
