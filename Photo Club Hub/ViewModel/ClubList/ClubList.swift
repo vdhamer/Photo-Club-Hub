efad5fb704c7375bf8493383d6638b97da54046f
Peter van den Hamer <github@vdhamer.com>
2023-12-17 17:58:38 +0100

    Extended ClubList https://www.shot71.nl https://www.fotokringbeeldhoek.nl

--- Photo Club Hub/ViewModel/ClubList/ClubList.swift
+++ Photo Club Hub/ViewModel/ClubList/ClubList.swift
@@ -7,12 +7,13 @@
 
 import SwiftyJSON
 import CoreData // for NSManagedObjectContext
+import CoreLocation // for CLLocationCoordinate2D
 
 let dataSourceURL: String = """
                             https://raw.githubusercontent.com/\
                             vdhamer/Photo-Club-Hub/\
                             main/\
-                            Photo%20Club%20Hub/JsonFiles/ClubList.json
+                            Photo%20Club%20Hub/ViewModel/ClubList/ClubList.json
                             """
 
 /* Example of basic ClubList.json content
@@ -62,30 +63,45 @@ class ClubList {
 
     private func readJSONClubList(bgContext: NSManagedObjectContext) {
 
+        ifDebugPrint("Starting readJSONClubList() in background")
+
         if let data = try? String(contentsOf: URL(string: dataSourceURL)!) {
             // give the data to SwiftyJSON to parse
             let jsonRoot = JSON(parseJSON: data) // call to SwiftyJSON
 
-            // read the commits back out
-            let jsonClubs = jsonRoot["clubs"].arrayValue
+            // extract the Clubs part of JSON string
+            let jsonClubs: [JSON] = jsonRoot["clubs"].arrayValue
+            ifDebugPrint("Found \(jsonClubs.count) clubs in file.")
 
-//            DispatchQueue.main.async { [unowned self] in
-            var count = 1
             for jsonClub in jsonClubs {
-                print("""
-                      Club#\(count): \
-                      \(jsonClub["idPlus"]["fullName"]), \
-                      \(jsonClub["idPlus"]["town"])
-                      """)
-                count += 1
-//                    let commit = Commit(context: self.container.viewContext) // create Commit object commit
-//                    self.fillCommitFields(commit: commit, usingJSON: jsonGithubCommit) // populate Commit object fields
+                let idPlus = PhotoClubIdPlus(fullName: jsonClub["idPlus"]["fullName"].stringValue,
+                                             town: jsonClub["idPlus"]["town"].stringValue,
+                                             nickname: jsonClub["idPlus"]["nickName"].stringValue)
+                ifDebugPrint("Adding Club# \(idPlus.fullName), \(idPlus.town), aka \(idPlus.nickname)")
+                let coordinates = CLLocationCoordinate2D(latitude: jsonClub["coordinates"]["latitude"].doubleValue,
+                                                         longitude: jsonClub["coordinates"]["longitude"].doubleValue)
+                let photoClubWebsite = URL(string: jsonClub["website"].stringValue)
+                let club = PhotoClub.findCreateUpdate(
+                                                      context: bgContext,
+                                                      photoClubIdPlus: idPlus,
+                                                      photoClubWebsite: photoClubWebsite,
+                                                      fotobondNumber: nil, kvkNumber: nil,
+                                                      coordinates: coordinates
+                                                     )
+                club.hasHardCodedMemberData = true // TODO needed?
+            }
+            do {
+                if bgContext.hasChanges {
+                    try bgContext.save() // commit all changes
                 }
-
-//                saveRecordsToDatabase()
-//            }
+                ifDebugPrint("Completed inserting/updated JSON ClubList in background")
+            } catch {
+                ifDebugFatalError("Failed to save changes to Core Data",
+                                  file: #fileID, line: #line) // likely deprecation of #fileID in Swift 6.0
+                // in release mode, the failed database update is only logged. App doesn't stop.
+            }
         } else {
-            fatalError("Please check URL \(dataSourceURL)")
+            ifDebugFatalError("Please check URL \(dataSourceURL)")
         }
 
     }
