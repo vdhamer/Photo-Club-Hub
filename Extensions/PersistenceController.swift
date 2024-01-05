1c4673044da1c5ad5a5b9ddf8ea9497199b8b76f
Peter van den Hamer <github@vdhamer.com>
2024-01-05 22:47:56 +0100

    automaticallyMergesChangesFromParent - moved statement a few lines to match the template in use by Apple for Core Data

--- Extensions/PersistenceController.swift
+++ Extensions/PersistenceController.swift
@@ -30,12 +30,12 @@ struct PersistenceController {
         // next line was added to the template code for PersistenceController in iOS 15.2
         // it is needed to refresh the display when online content was merged to the core data database
         // but gives warning "View context accessed for persistent container <name> with no stores loaded
-        container.viewContext.automaticallyMergesChangesFromParent = true
         container.loadPersistentStores(completionHandler: { (_ storeDescription, error) in
 			if let error = (error as NSError?) {
 				fatalError("Unresolved error \(error), \(error.userInfo)") // don't know how to recover
             }
 		})
+        container.viewContext.automaticallyMergesChangesFromParent = true
 	}
 
     func save() {
