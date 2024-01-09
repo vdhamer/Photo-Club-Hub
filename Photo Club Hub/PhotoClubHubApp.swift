b44cd420d96480dd7d9694d2a408bbde8ab4553a
Peter van den Hamer <github@vdhamer.com>
2024-01-09 23:27:57 +0100

    Removed line of dead code

--- Photo Club Hub/PhotoClubHubApp.swift
+++ Photo Club Hub/PhotoClubHubApp.swift
@@ -33,9 +33,6 @@ struct PhotoClubHubApp: App {
                 .environment(\.managedObjectContext, PersistenceController.shared.container.viewContext) // main queue!
                 .onAppear {
                     PhotoClubHubApp.loadClubsAndMembers()
-
-                    let foregroundContext = PersistenceController.shared.container.viewContext // UI context = main
-//                    try? foregroundContext.save() // moves data to persistent store  TODO needed?
                 }
         }
         .onChange(of: scenePhase) { // pre-iOS 17 there was 1 param. Since iOS 17 it is 0 or 2.
