898fe8a53204429c30e7f68fec0b7d55cf726c0b
Peter van den Hamer <github@vdhamer.com>
2024-07-26 23:03:36 +0200

    Localization cleanup

---
+++ Photo Club Hub/ViewModel/MemberPortfolio/MemberOptionalFields.swift
@@ -0,0 +1,19 @@
+//
+//  MemberOptionalFields.swift
+//  Photo Club Hub
+//
+//  Created by Peter van den Hamer on 26/07/2024.
+//
+
+import Foundation // for URL
+
+// Set of paraameters (with their defaults) used when creating structs of type MemberPortfolio.
+// Note that MemberPortfolio is also an entity type in the CoreData database.
+struct MemberOptionalFields {
+    var memberRolesAndStatus: MemberRolesAndStatus =
+        MemberRolesAndStatus(role: [:], status: [:]) // defaults to an empty pair of dictionaries instead of to nil
+    var dateInterval: DateInterval?
+    var memberWebsite: URL?
+    var latestImage: URL?
+    var latestThumbnail: URL?
+}
---
+++ Photo Club Hub/ViewModel/OrganizationOptionalFields.swift
@@ -0,0 +1,8 @@
+//
+//  OrganizationOptionalFields.swift
+//  Photo Club Hub
+//
+//  Created by Peter van den Hamer on 26/07/2024.
+//
+
+import Foundation
