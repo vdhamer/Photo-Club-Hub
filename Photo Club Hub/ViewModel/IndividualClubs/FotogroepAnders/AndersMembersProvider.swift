88c90c9ed86184b68944b75ae003a18f27955d78
Peter van den Hamer <github@vdhamer.com>
2024-07-27 21:44:44 +0200

    Removed hardcoded membership lists for fgAnders and fgDeGender

--- Photo Club Hub/ViewModel/IndividualClubs/FotogroepAnders/AndersMembersProvider.swift
+++ Photo Club Hub/ViewModel/IndividualClubs/FotogroepAnders/AndersMembersProvider.swift
@@ -13,7 +13,6 @@ class AndersMembersProvider {
 
         // Photogroup Anders doesn't currently support loading member data from an online site.
         // So we only insert a member or two from info hardcoded in insertSomeHardcodedMemberData()
-//        insertSomeHardcodedMemberData(bgContext: bgContext)
         insertOnlineMemberData(bgContext: bgContext) // should do its own bgContext.save()
     }
 
--- Photo Club Hub/ViewModel/IndividualClubs/FotogroepDeGender/FotogroepDeGenderMembersProvider.swift
+++ Photo Club Hub/ViewModel/IndividualClubs/FotogroepDeGender/FotogroepDeGenderMembersProvider.swift
@@ -13,7 +13,6 @@ class FotogroepDeGenderMembersProvider {
 
         // Photo club de Gender doesn't currently support loading member data from an online site.
         // So we only insert a member or two from info hardcoded in insertSomeHardcodedMemberData()
-//        insertSomeHardcodedMemberData(bgContext: bgContext) // does its own bgContext.save()
         insertOnlineMemberData(bgContext: bgContext) // should do its own bgContext.save()
     }
 
