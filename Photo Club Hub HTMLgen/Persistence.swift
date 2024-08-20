67cd6b7565dc1b05422c7907d50810c7881e5483
Peter van den Hamer <13396568+vdhamer@users.noreply.github.com>
2024-08-20 23:07:00 +0200

    Fix: #450

---
+++ Photo Club Hub HTMLgen/Persistence.swift
@@ -0,0 +1,57 @@
+//
+//  Persistence.swift
+//  Photo Club Hub HTMLgen
+//
+//  Created by Peter van den Hamer on 20/08/2024.
+//
+
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+
+    @MainActor
+    static let preview: PersistenceController = {
+        let result = PersistenceController(inMemory: true)
+        let viewContext = result.container.viewContext
+        for _ in 0..<10 {
+            let newItem = Item(context: viewContext)
+            newItem.timestamp = Date()
+        }
+        do {
+            try viewContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+        return result
+    }()
+
+    let container: NSPersistentContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "Photo_Club_Hub_HTMLgen")
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+}
---
+++ Photo Club Hub HTMLgen/Photo_Club_Hub_HTMLgenApp.swift
@@ -0,0 +1,20 @@
+//
+//  Photo_Club_Hub_HTMLgenApp.swift
+//  Photo Club Hub HTMLgen
+//
+//  Created by Peter van den Hamer on 20/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Photo_Club_Hub_HTMLgenApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
