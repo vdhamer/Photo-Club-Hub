ccca5e5a49f2025210574d53855ab513d165ba05
Peter van den Hamer <13396568+vdhamer@users.noreply.github.com>
2025-02-05 21:52:26 +0100

    Changed order in which the 3 Bools are presented here. Just for clarity.
    Former-commit-id: 60a2416cf7963896a8b4d3e48d6f2578e73dab76

--- Photo Club Hub/Extensions/Settings.swift
+++ Photo Club Hub/Extensions/Settings.swift
@@ -9,6 +9,18 @@ import Foundation
 
 struct Settings {
 
+    static var dataResetPending: Bool { // stored as a string shown in Settings, controlled by version and any resets
+        // returns true until a reset is done
+
+        if UserDefaults.standard.object(forKey: "dataResetPending") == nil {
+            UserDefaults.standard.set(true, forKey: "dataResetPending") // interpret nil as true
+        }
+
+        let prevValue = UserDefaults.standard.bool(forKey: "dataResetPending")
+        UserDefaults.standard.set(false, forKey: "dataResetPending") // never true more than once
+        return prevValue // if true, app has to react immediately (by executing data reset)
+    } // implicit getter only
+
     static var manualDataLoading: Bool { // controlled by toggle in Settings
         // Setting this to true clears the existing database and skips loading any data on app startup.
         // It displays "Manual loading" in the Prelude startup screen as a warning that the mode is set.
@@ -32,17 +44,5 @@ struct Settings {
             UserDefaults.standard.set(newValue, forKey: "extraCoreDataSaves")
         }
     }
-
-    static var dataResetPending: Bool { // stored as a string shown in Settings, controlled by version and any resets
-        // returns true until a reset is done
-
-        if UserDefaults.standard.object(forKey: "dataResetPending") == nil {
-            UserDefaults.standard.set(true, forKey: "dataResetPending") // interpret nil as true
-        }
-
-        let prevValue = UserDefaults.standard.bool(forKey: "dataResetPending")
-        UserDefaults.standard.set(false, forKey: "dataResetPending") // never true more than once
-        return prevValue // if true, app has to react immediately (by executing data reset)
-    } // implicit getter only
 
 }
