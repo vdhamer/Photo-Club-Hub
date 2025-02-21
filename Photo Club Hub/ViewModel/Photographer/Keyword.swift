216499626fa35ea9da27805cbfd8f9f6498cbb48
Peter van den Hamer <13396568+vdhamer@users.noreply.github.com>
2025-02-21 23:25:59 +0100

    Added Keyword.swift
    Former-commit-id: 299a3b2c8e47a7904de0d5c24def9050723b5b14

--- Photo Club Hub/ViewModel/Photographer/Keyword.swift
+++ Photo Club Hub/ViewModel/Photographer/Keyword.swift
@@ -5,4 +5,134 @@
 //  Created by Peter van den Hamer on 19/02/2025.
 //
 
-import Foundation
+import CoreData // for NSManagedObjectContext
+
+extension Keyword {
+
+    // `id` is persisted in CoreData as `id_` but has to be public because it is the `id` for the Identifiable protocol
+    public var id: String {
+        get {
+            if id_ != nil { // shouldn't be nil in the first place
+                return id_!
+            } else {
+                ifDebugFatalError("CoreData id_ for Keyword is nil", file: #fileID, line: #line)
+                return "No id for Keyword"
+            }
+        }
+        set {
+            id_ = newValue.capitalized // ensure CoreData always contains string with first letter capitalized
+        }
+    }
+
+    // MARK: - find or create
+
+    // Find existing Keyword object or create a new one.
+    // Update existing attributes or fill the new object
+    fileprivate static func findCreateUpdate(context: NSManagedObjectContext, // can be foreground of background context
+                                             id: String,
+                                             isStandard: Bool
+                                            ) -> Keyword {
+
+        // execute fetchRequest to get keyword object for id=id. Query could return more than 1.
+        let predicateFormat: String = "id_ = %@" // avoid localization
+        let predicate = NSPredicate(format: predicateFormat, argumentArray: [id])
+        let fetchRequest: NSFetchRequest<Keyword> = Keyword.fetchRequest()
+        fetchRequest.predicate = predicate
+        let keywords: [Keyword] = (try? context.fetch(fetchRequest)) ?? [] // nil = absolute failure
+
+        if keywords.count > 1 { // there is actually a Core Data constraint to prevent this
+            ifDebugFatalError("Query returned multiple (\(keywords.count)) Keywords with code \(id)",
+                              file: #fileID, line: #line) // likely deprecation of #fileID in Swift 6.0
+            // in release mode, log that there are multiple clubs, but continue using the first one.
+        }
+
+        if let keyword = keywords.first { // already exists, so update non-identifying attributes
+            if keyword.update(context: context, isStandard: isStandard) {
+                print("Updated info for keyword \"\(keyword.id)\" that became \(isStandard ? "" : "non-")standard")
+                if Settings.extraCoreDataSaves {
+                    save(context: context, keyword: keyword, create: false)
+                }
+            }
+            return keyword
+        } else {
+            // cannot use Language() initializer because we must use supplied context
+            let entity = NSEntityDescription.entity(forEntityName: "Keyword", in: context)!
+            let keyword = Keyword(entity: entity, insertInto: context)
+            keyword.id = id // immediately set it to a non-nil value
+            _ = keyword.update(context: context, isStandard: isStandard) // ignore whether update made changes
+            if Settings.extraCoreDataSaves {
+                save(context: context, keyword: keyword, create: true)
+            }
+            print("Created new Keyword for \"\(keyword.id)\"")
+            return keyword
+        }
+
+    }
+
+    // Find existing non-standard Keyword object or create a new one.
+    // Update existing attributes or fill the new object
+    static func findCreateUpdateStandard(context: NSManagedObjectContext, // can be foreground of background context
+                                         id: String
+                                        ) -> Keyword {
+        findCreateUpdate(context: context, id: id, isStandard: true)
+    }
+
+    // Find existing non-standard Keyword object or create a new one.
+    // Update existing attributes or fill the new object
+    static func findCreateUpdateNonStandard(context: NSManagedObjectContext, // can be foreground of background context
+                                            id: String
+                                           ) -> Keyword {
+        findCreateUpdate(context: context, id: id, isStandard: false)
+    }
+
+    // Update non-identifying attributes/properties within an existing instance of class Keyword if needed.
+    // Returns whether an update was needed.
+    fileprivate func update(context: NSManagedObjectContext,
+                            isStandard: Bool) -> Bool {
+
+        var modified: Bool = false
+
+        if self.isStandard != isStandard {
+            self.isStandard = isStandard
+            modified = true
+        }
+
+        if modified && Settings.extraCoreDataSaves {
+            do {
+                try context.save() // update modified properties of a Keyword object
+             } catch {
+                 ifDebugFatalError("Update failed for Keyword \"\(id)\"",
+                                  file: #fileID, line: #line) // likely deprecation of #fileID in Swift 6.0
+                // in release mode, if save() fails, just continue
+            }
+        }
+
+        return modified
+    }
+
+    // MARK: - find or create
+
+    func delete(context: NSManagedObjectContext) { // for testing?
+        context.delete(self)
+        do {
+            try context.save()
+        } catch {
+            context.rollback()
+            ifDebugFatalError("Could not save the deletion of Keyword \"\(self.id)\"")
+        }
+    }
+
+    fileprivate static func save(context: NSManagedObjectContext, keyword: Keyword, create: Bool) {
+        do {
+            try context.save()
+        } catch {
+            context.rollback()
+            if create {
+                ifDebugFatalError("Could not save created Keyword \"\(keyword.id)\"")
+            } else {
+                ifDebugFatalError("Could not save updated property of Keyword \"\(keyword.id)\"")
+            }
+        }
+    }
+
+}
