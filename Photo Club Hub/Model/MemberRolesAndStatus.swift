07911369340c731176e31fd66a64ab33bde39e4f
Peter van den Hamer <13396568+vdhamer@users.noreply.github.com>
2025-04-07 16:52:15 +0200

    String catalog refactoring Moved admin/chairman/secretary/treasurer/viceChaiman/other from Localizable.xcstrings (default) to Package.xcstrings

--- Photo Club Hub/Model/MemberRolesAndStatus.swift
+++ Photo Club Hub/Model/MemberRolesAndStatus.swift
@@ -24,17 +24,17 @@ enum MemberRole {
 
         switch self {
         case .admin:
-            return String(localized: "admin", comment: translateComment)
+            return String(localized: "admin", table: "Package", comment: translateComment)
         case .chairman:
-            return String(localized: "chairman", comment: translateComment)
+            return String(localized: "chairman", table: "Package", comment: translateComment)
         case .secretary:
-            return String(localized: "secretary", comment: translateComment)
+            return String(localized: "secretary", table: "Package", comment: translateComment)
         case .treasurer:
-            return String(localized: "treasurer", comment: translateComment)
+            return String(localized: "treasurer", table: "Package", comment: translateComment)
         case .viceChairman:
-            return String(localized: "vice-chairman", comment: translateComment) // used in fgWaalre
+            return String(localized: "vice-chairman", table: "Package", comment: translateComment) // used in fgWaalre
         case .other:
-            return String(localized: "other", comment: translateComment) // used in fgDeGender
+            return String(localized: "other", table: "Package", comment: translateComment) // used in fgDeGender
         }
     }
 }
