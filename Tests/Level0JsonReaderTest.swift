2943e1e4293ebcda4850b0a6c4c1a8e8571f819f
Peter van den Hamer <13396568+vdhamer@users.noreply.github.com>
2025-05-10 22:11:41 +0200

    Minor update to test results

--- Tests/Level0JsonReaderTest.swift
+++ Tests/Level0JsonReaderTest.swift
@@ -75,7 +75,7 @@ import CoreData // for NSManagedObjectContext
 
         _ = Level0JsonReader(bgContext: bgContext, // read root.Level0.json file
                              useOnlyFile: false)
-        #expect(Keyword.count(context: bgContext) == 15)
+        #expect(Keyword.count(context: bgContext) == 19)
     }
 
     // Read language.level0.json.
