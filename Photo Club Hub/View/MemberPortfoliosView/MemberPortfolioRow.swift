14f5fd1d4134327ba9753ce45a894653cef7a0ec
Peter van den Hamer <13396568+vdhamer@users.noreply.github.com>
2025-06-09 15:13:40 +0200

    Fixed bug in processing of Expertises

--- Photo Club Hub/View/MemberPortfoliosView/MemberPortfolioRow.swift
+++ Photo Club Hub/View/MemberPortfoliosView/MemberPortfolioRow.swift
@@ -96,40 +96,40 @@ struct MemberPortfolioRow: View {
     fileprivate func localizeSortAndClip(moc: NSManagedObjectContext, _ photographerKeywords: Set<PhotographerKeyword>)
     -> LocalizedExpertiseResultLists {
 
-        // first translate keywords to appropriate language and make elements non-optional
-        var interim: [LocalizedKeywordResult] = [] // start with empty array
+        // Step 1. Translate keywords to appropriate language
+        var translated: [LocalizedKeywordResult] = [] // start with empty array
         for photographerKeyword in photographerKeywords {
-            interim.append(photographerKeyword.keyword.selectedLocalizedKeyword) // choose appropriate language
+            translated.append(photographerKeyword.keyword.selectedLocalizedKeyword) // choose most suitable language
         }
 
-        // next sort based on selected language.  Has special behavior for keywords without translation
-        let sorted: [LocalizedKeywordResult] = interim.sorted() // note dedicated LocalizedKeywordResult.<() function
+        // Step 2. Sort based on selected language.  Has special behavior for keywords without translation
+        let sorted: [LocalizedKeywordResult] = translated.sorted() // note dedicated LocalizedKeywordResult.<() function
 
-        // limit size to maxKeywordsPerMember displayed keywords
+        // Step 3. Clip size to maxKeywordsPerMember keywords
         var clipped: [LocalizedKeywordResult] = [] // start with empty array
         if sorted.count > 0 {
-            for index in 1...min(maxKeywordsPerMember, sorted.count) {
-                clipped.append(sorted[index-1]) // copy the (aphabetically) first few LocalizedKeywordResult elements
+            for index in 0...min(maxKeywordsPerMember-1, sorted.count-1) {
+                clipped.append(sorted[index]) // copy the first few sorted LocalizedKeywordResult elements
             }
         }
-        if sorted.count > maxKeywordsPerMember {
-            let moreKeyword = Keyword.findCreateUpdateStandard(
-                context: moc,
-                id: String(localized: "Too many expertises",
-                           table: "Localizable",
-                           comment: "Shown if too many keywords found"),
-                name: [],
-                usage: [] )
+        if sorted.count > maxKeywordsPerMember { // if list overflows, add a warning
+            let moreKeyword = Keyword.findCreateUpdateNonStandard(
+                                        context: moc,
+                                        id: String(localized: "Too many expertises",
+                                                   table: "Localizable",
+                                                   comment: "Shown when too many expertises are found"),
+                                        name: [],
+                                        usage: [] )
             let moreLocalizedKeyword: LocalizedKeywordResult = moreKeyword.selectedLocalizedKeyword
             clipped.append(LocalizedKeywordResult(localizedKeyword: moreLocalizedKeyword.localizedKeyword,
                                                   id: moreKeyword.id,
                                                   customHint: customHint(localizedKeywordResults: sorted)))
         }
 
-        // insert delimeters where needed
+        // Step 4. Split list of photographer's expertises into 2 parts: standard and nonStandard
         var standard: [LocalizedKeywordResult] = [] // start with two empty arrays
         var nonStandard: [LocalizedKeywordResult] = []
-        for item in sorted {
+        for item in clipped {
             if item.isStandard { // turn this into ["keywordA,", "keywordB,", "keywordC,"]
                 standard.append(item) // accept appending "," to item
             } else {
