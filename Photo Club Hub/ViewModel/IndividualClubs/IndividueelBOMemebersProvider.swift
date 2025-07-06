238a642f7c2c1440697fb66d27494a5c8829f764
Peter van den Hamer <13396568+vdhamer@users.noreply.github.com>
2025-07-06 23:36:11 +0200

    Fix: #566 new club IndividueelBO Club is hidden behind switch in settings (for now)

---
+++ Photo Club Hub/ViewModel/IndividualClubs/IndividueelBOMemebersProvider.swift
@@ -0,0 +1,51 @@
+//
+//  IndividueelBOMemebersProvider.swift
+//  Photo Club Hub
+//
+//  Created by Peter van den Hamer on 06/07/2025.
+//
+
+import CoreData // for PersistenceController
+
+public class IndividueelBOMembersProvider {
+
+    public init(bgContext: NSManagedObjectContext,
+                useOnlyInBundleFile: Bool = false,
+                synchronousWithRandomTown: Bool = false,
+                randomTown: String = "RandomTown") {
+
+        if synchronousWithRandomTown {
+            bgContext.performAndWait { // ...or execute same block synchronously
+                insertOnlineMemberData(bgContext: bgContext, town: randomTown)
+            }
+        } else {
+            bgContext.perform { // execute block asynchronously...
+                self.insertOnlineMemberData(bgContext: bgContext)
+            }
+        }
+
+    }
+
+    fileprivate func insertOnlineMemberData(bgContext: NSManagedObjectContext, town: String = "Eindhoven") {
+        let idPlus = OrganizationIdPlus(fullName: "Individuele Leden Brabant Oost",
+                                        town: town,
+                                        nickname: "IndividueelBO")
+
+        let club = Organization.findCreateUpdate(context: bgContext,
+                                                 organizationTypeEnum: .club,
+                                                 idPlus: idPlus)
+        ifDebugPrint("\(club.fullNameTown): Starting insertOnlineMemberData() in background")
+
+        _ = Level2JsonReader(bgContext: bgContext,
+                             organizationIdPlus: idPlus,
+                             isInTestBundle: false,
+                             useOnlyInBundleFile: false)
+        do {
+            try bgContext.save()
+        } catch {
+            ifDebugFatalError("Failed to save club \(idPlus.nickname)", file: #fileID, line: #line)
+        }
+
+    }
+
+}
