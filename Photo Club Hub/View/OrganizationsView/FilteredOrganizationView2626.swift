835cb68655b7ef389941c163324b05ef4c84f2dc
vdhamer <13396568+vdhamer@users.noreply.github.com>
2026-02-06 18:58:39 +0100

    Fix: #673

--- Photo Club Hub/View/OrganizationsView/FilteredOrganizationView2626.swift
+++ Photo Club Hub/View/OrganizationsView/FilteredOrganizationView2626.swift
@@ -302,36 +302,52 @@ struct FilteredOrganizationView2626: View {
 @available(iOS 26.0, *)
 extension FilteredOrganizationView2626 { // graphic representation
 
-    /// Returns the tint color for a map marker representing an organization. // TODO backport function to v1718
+    /// Returns the tint color for a single map marker representing an organization.
+    /// The rules for this are executed in a specific priority.
+    /// Note that the marker has a different symbol for Clubs than for Museums.
     ///
     /// - Parameters:
-    ///   - organization: The organization for which the function is asked to determine the marker tint.
+    ///   - organization: The organization for which to return the marker tint.
     ///   - selectedOrganization: The organization currently centered/selected on this particular  map.
-    /// - Returns: A `Color` used to tint the marker for the given organization.
+    /// - Returns: The `Color` in which to tint the marker for `organization`.
     private func selectMarkerTint(organization: Organization, selectedOrganization: Organization) -> Color {
+        let errorColor = Color.orange
 
+        /// The marker for `selectedOrganization` gets a special color. It is usually at the center of the map.
         if isEqual(organizationLHS: organization, organizationRHS: selectedOrganization) {
-            .organizationColor // this is the organization centered on this particular map
-        } else if organization.organizationType.isUnknown {
-            .red // for .unknown organization type (has higher priority than other rules)
+            return .organizationColor
+        }
 
-            guard !(appSettings.highlightNonFotobondNL && appSettings.highlightFotobondNL) else {
+        if organization.organizationType.isMuseum {
+            return .blue /// Show any Museum in blue (except the `selectedOrganization`)
+        }
+
+        guard organization.organizationType.isUnknown == false else {
+            ifDebugFatalError("Unexpected organizationType in selectMarkerTint().") // falls though in production builds
+            return errorColor // for .unknown organization type (has higher priority than other rules)
+        }
+
+        if organization.organizationType.isClub {
+
+            let appSettings = PreferencesViewModel().preferences /// persisted via UserDefaults via `@Published``
+
+            guard !(appSettings.highlightNonFotobondNL == true && appSettings.highlightFotobondNL == true) else {
                 ifDebugFatalError("Fotobond and non-Fotobond toggle are both enabled. That shouldn't happen.")
-                return .orange // error color
+                return errorColor
             }
 
-            let clubInFotobond: Bool = (organization.fotobondClubNumber?.id != nil)
+            let clubInFotobond: Bool = (organization.fotobondClubNumber?.id != nil) // is club member of Dutch Fotobond
             let highlightColor: Color = appSettings.highlightColor
 
             if appSettings.highlightFotobondNL {
-                return clubInFotobond ? highlightColor : .gray // highlight Fotobond clubs, other clubs in gray
+                return clubInFotobond ? highlightColor : .gray // highlight Fotobond clubs, and make other clubs gray
             } else {
-                return clubInFotobond ? .gray: highlightColor // highlight nonFotobond clubs, other clubs in gray
+                return clubInFotobond ? .gray: highlightColor // highlight NonFotobond clubs, and make other clubs gray
             }
         }
 
-        ifDebugFatalError("Unknown organizationType in selectMarkerTint()")
-        return .orange // organization.organizationType.isUnknown
+        return .blue // fall through isn't really possible (we have checked all enum cases)
+
     }
 
     private func systemName(organizationType: OrganizationType?, circleNeeded: Bool) -> String { // for SF symbols
