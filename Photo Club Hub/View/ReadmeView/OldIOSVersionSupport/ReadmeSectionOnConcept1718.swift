c16d6d8bebec3fa8b90f8579f85d8dc055718700
vdhamer <13396568+vdhamer@users.noreply.github.com>
2026-04-25 00:49:23 +0200

    Minor localization cleanup

---
+++ Photo Club Hub/View/ReadmeView/OldIOSVersionSupport/ReadmeSectionOnConcept1718.swift
@@ -0,0 +1,18 @@
+//
+//  ReadmeSectionOnConcept1718.swift
+//  Photo Club Hub
+//
+//  Created by Peter van den Hamer on 24/04/2026.
+//
+
+import SwiftUI
+
+struct ReadmeSectionOnConcept1718: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReadmeSectionOnConcept1718()
+}
