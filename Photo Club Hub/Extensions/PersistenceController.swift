46cbeaa776b0cb43a23b385fe65f4e081af635fc
vdhamer <13396568+vdhamer@users.noreply.github.com>
2026-05-08 15:21:33 +0200

    Fixed merge issue

--- Photo Club Hub/Extensions/PersistenceController.swift
+++ Photo Club Hub/Extensions/PersistenceController.swift
@@ -72,7 +72,7 @@ public struct PersistenceController: Sendable {
                 optionalFields: PhotographerOptionalFields(
                     bornDT: Date() - Double.random(in: 365*24*3600 ... 75*365*24*3600),
                     isDeceased: memberRolesAndStatus.isDeceased(),
-                    photographerWebsite: URL(string: "https://www.example.com/JanDEau\(index)"),
+                    photographerWebsite: URL(string: "https://www.example.com/JanDEau\(photographerIndex)"),
                     photographerImage: URL(string:
                                             "https://www.fcDeGender.nl/wp-content/uploads/Peter-van-den-Hamer.png")
                 )
