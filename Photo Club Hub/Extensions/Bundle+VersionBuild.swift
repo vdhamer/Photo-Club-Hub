5054ca2f868ff9618b48875462f43a3172d9fb23
vdhamer <13396568+vdhamer@users.noreply.github.com>
2026-08-22 14:25:15 +0200

    Computed properties providing version, build, commit info for the app.

--- Photo Club Hub/Extensions/Bundle+VersionBuild.swift
+++ Photo Club Hub/Extensions/Bundle+VersionBuild.swift
@@ -9,25 +9,25 @@ import Foundation
 
 extension Bundle {
 
-    var shortVersion: String {
+    var shortVersion: String { // e.g. "2.12.3"
         if let result = infoDictionary?["CFBundleShortVersionString"] as? String {
             return result
         } else {
             assert(false)
-            return ""
+            return "No version#"
         }
     }
 
-    var buildVersion: String {
+    var buildVersion: String { // e.g. "1234"
         if let result = infoDictionary?["CFBundleVersion"] as? String {
             return result
         } else {
             assert(false)
-            return ""
+            return "No build#"
         }
     }
 
-    var fullVersion: String {
+    var fullVersion: String { // e.g. "2.12.3 (1234)"
         return "\(shortVersion) (\(buildVersion))"
     }
 
