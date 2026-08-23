2e04ca7741f9523eae465f9d8d6d34a7012c1c4c
vdhamer <13396568+vdhamer@users.noreply.github.com>
2026-08-23 20:02:24 +0200

    Localization of empty-result warnings.

---
+++ Photo Club Hub/View/SharedView/CalloutBox.swift
@@ -0,0 +1,8 @@
+//
+//  CalloutBox.swift
+//  Photo Club Hub
+//
+//  Created by Peter van den Hamer on 23/08/2026.
+//
+
+import Foundation
---
+++ Photo Club Hub/View/SharedView/EmptyListReason.swift
@@ -0,0 +1,8 @@
+//
+//  EmptyListReason.swift
+//  Photo Club Hub
+//
+//  Created by Peter van den Hamer on 23/08/2026.
+//
+
+import Foundation
